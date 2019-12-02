43df6a7f0d334668040ed5b75f085cb0468de332
Razvan Cristea <razcristea@gmail.com>
2019-12-02 14:32:44 +0200

    first commit

---
+++ src/App.js
@@ -0,0 +1,97 @@
+import React from 'react';
+import marked from 'marked'
+import './App.css';
+
+class App extends React.Component {
+  constructor(){
+    super()
+    this.state = {
+      markdown:''
+    }
+    this.handleChange = this.handleChange.bind(this)
+    this.getMarkdownText = this.getMarkdownText.bind(this)
+  }
+   
+    getMarkdownText() {
+      marked.setOptions({
+  breaks: true,
+});
+    let textToMark = this.state.markdown.toString();
+    let rawMarkup = marked(textToMark, {sanitize: true});
+    return { __html: rawMarkup };
+  }
+  componentDidMount(){
+    const defaultState = `# Welcome to my React Markdown Previewer!
+
+## This is a sub-heading...
+### And here's some other cool stuff:
+  
+Heres some code, \`<div></div>\`, between 2 backticks.
+
+\`\`\`
+// this is multi-line code:
+
+function anotherExample(firstLine, lastLine) {
+  if (firstLine == '\`\`\`' && lastLine == '\`\`\`') {
+    return multiLineCode;
+  }
+}
+\`\`\`
+  
+You can also make text **bold**... whoa!
+Or _italic_.
+Or... wait for it... **_both!_**
+And feel free to go crazy ~~crossing stuff out~~.
+
+There's also [links](https://www.freecodecamp.com), and
+> Block Quotes!
+
+And if you want to get really crazy, even tables:
+
+Wild Header | Crazy Header | Another Header?
+------------ | ------------- | ------------- 
+Your content can | be here, and it | can be here....
+And here. | Okay. | I think we get it.
+
+- And of course there are lists.
+  - Some are bulleted.
+     - With different indentation levels.
+        - That look like this.
+
+
+1. And there are numbererd lists too.
+1. Use just 1s if you want! 
+1. But the list goes on...
+- Even if you use dashes or asterisks.
+* And last but not least, let's not forget embedded images:
+
+![React Logo w/ Text](https://goo.gl/Umyytc)
+`
+    this.setState({
+      markdown: defaultState
+    })
+  }
+
+
+  handleChange(event){
+    this.setState({
+      markdown: event.target.value
+    });
+
+    }
+  
+  
+  render() {
+    return (
+      <>
+        <textarea id="editor" value={this.state.markdown} onChange={this.handleChange}></textarea>
+        <div dangerouslySetInnerHTML={this.getMarkdownText()} id="preview" />
+      </>
+    );
+  }
+}
+
+
+
+
+export default App;
